fb2944e051e8be1313308a1ab346e0e2e2432653
ibrahimmutua <Ibrahimmutua315@gmail.com>
2025-09-17 13:29:24 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+op = input("Enter operation (+, -, *, /): ")
+
+if op == "+":
+    print(num1, "+", num2, "=", num1 + num2)
+elif op == "-":
+    print(num1, "-", num2, "=", num1 - num2)
+elif op == "*":
+    print(num1, "*", num2, "=", num1 * num2)
+elif op == "/":
+    print(num1, "/", num2, "=", num1 / num2)
+else:
+    print("Invalid operation")
